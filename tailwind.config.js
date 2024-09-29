39153e571c2d818d5e980300d7eb5662f4118db9
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-29 13:19:31 +0530

    frist

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins : ["Poppins", 'system-ui'],
+      },
+    },
+    colors:{
+      'bg-color': '#091420',
+      'bg-dark-color' : '#0c1a28',
+      'primary-color': '#4fe98c',
+    },
+    width:{
+      'custom-image-width':'200px',
+    },
+  },
+  plugins: [],
+}
+
